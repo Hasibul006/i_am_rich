981a767f806cd87408adfa70a58d44202e4e6013
Hasibul006 <hi006kuet@gmail.com>
2024-03-30 11:52:52 +0600

    am i rich?

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.white38,
+      backgroundColor: Colors.blueGrey[900],
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         title: Text('I am rich'),
